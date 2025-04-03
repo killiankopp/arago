4f94ddd869ca73fb4d909c89a80449920d0e603c
Killian KOPP <killiankopp@gmail.com>
2025-04-03 15:05:24 +0200

    add ad impression tracking via gRPC integration

--- ad/go.mod
+++ ad/go.mod
@@ -4,23 +4,24 @@ go 1.23.1
 
 require (
 	github.com/google/uuid v1.6.0
+	github.com/killiankopp/arago/tracker v0.0.0-20250403114306-9a7caf2148d8
 	go.mongodb.org/mongo-driver v1.17.3
 	google.golang.org/grpc v1.71.1
 	google.golang.org/protobuf v1.36.6
 )
 
 require (
-	github.com/golang/snappy v0.0.4 // indirect
-	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/golang/snappy v1.0.0 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
-	golang.org/x/crypto v0.32.0 // indirect
-	golang.org/x/net v0.34.0 // indirect
-	golang.org/x/sync v0.10.0 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/sync v0.12.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250324211829-b45e905df463 // indirect
 )
--- ad/server/service/ad.go
+++ ad/server/service/ad.go
@@ -6,11 +6,13 @@ import (
 	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"log"
 
 	pb "github.com/killiankopp/arago/ad/proto"
+	trackerpb "github.com/killiankopp/arago/tracker/proto"
 )
 
 type AdService struct {
@@ -25,33 +27,79 @@ func NewAdService(collection *mongo.Collection) *AdService {
 func (s *AdService) CreateAd(ctx context.Context, req *pb.CreateAdRequest) (*pb.CreateAdResponse, error) {
 	ad := req.Ad
 	ad.Uuid = uuid.New().String()
-	_, err := s.collection.InsertOne(ctx, bson.M{
-		"_id":         ad.Uuid,
-		"title":       ad.Title,
-		"description": ad.Description,
-		"url":         ad.Url,
-	})
+	err := s.insertAd(ctx, ad)
 	if err != nil {
 		return nil, err
 	}
 	return &pb.CreateAdResponse{Uuid: ad.Uuid}, nil
 }
 
+func (s *AdService) insertAd(ctx context.Context, ad *pb.Ad) error {
+	_, err := s.collection.InsertOne(ctx, bson.M{
+		"_id":         ad.Uuid,
+		"title":       ad.Title,
+		"description": ad.Description,
+		"url":         ad.Url,
+	})
+	return err
+}
+
 func (s *AdService) ReadAd(ctx context.Context, req *pb.AdRequest) (*pb.AdResponse, error) {
 	log.Printf("ReadAd called with UUID: %s", req.Uuid)
 
+	ad, err := s.findAdByUUID(ctx, req.Uuid)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.AdResponse{Ad: ad}, nil
+}
+
+func (s *AdService) findAdByUUID(ctx context.Context, uuid string) (*pb.Ad, error) {
 	var ad pb.Ad
-	err := s.collection.FindOne(ctx, bson.M{"_id": req.Uuid}).Decode(&ad)
+	err := s.collection.FindOne(ctx, bson.M{"_id": uuid}).Decode(&ad)
 	if err != nil {
 		if errors.Is(err, mongo.ErrNoDocuments) {
 			return nil, status.Errorf(codes.NotFound, "ad not found")
 		}
 		return nil, status.Errorf(codes.Internal, "failed to read ad: %v", err)
 	}
-	return &pb.AdResponse{Ad: &ad}, nil
+	return &ad, nil
 }
 
 func (s *AdService) ServeAd(ctx context.Context, req *pb.AdRequest) (*pb.AdResponse, error) {
-	// Implement the logic to serve an ad
-	return &pb.AdResponse{Ad: &pb.Ad{Uuid: req.Uuid}}, nil
+	log.Printf("ServeAd called with UUID: %s", req.Uuid)
+
+	ad, err := s.findAdByUUID(ctx, req.Uuid)
+	if err != nil {
+		return nil, err
+	}
+
+	err = s.trackImpression(ctx, req.Uuid)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.AdResponse{Ad: ad}, nil
+}
+
+func (s *AdService) trackImpression(ctx context.Context, adUUID string) error {
+	conn, err := grpc.Dial("localhost:50052", grpc.WithInsecure())
+	if err != nil {
+		log.Printf("Failed to connect to tracker service: %v", err)
+		return status.Errorf(codes.Internal, "failed to connect to tracker service: %v", err)
+	}
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+
+		}
+	}(conn)
+
+	trackerClient := trackerpb.NewTrackerServiceClient(conn)
+	_, err = trackerClient.UpdateImpression(ctx, &trackerpb.UpdateImpressionRequest{AdUuid: adUUID})
+	if err != nil {
+		log.Printf("Failed to update impression for ad_uuid %s: %v", adUUID, err)
+		return status.Errorf(codes.Internal, "failed to update impression: %v", err)
+	}
+	return nil
 }
